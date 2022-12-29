c4d6fc77b5672e4b715f50ca583f6a486ce473c3
Paul Hammant <paul@hammant.org>
2022-12-29 09:53:39 +0000

    don't like these components after all - should use MUI ones

--- libs/dashboard/components/src/lib/simple/check-box/check-box.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CheckBox from "./index";
-import { ComponentMeta } from "@storybook/react";
-
-export default {
-  component: CheckBox,
-} as ComponentMeta<typeof CheckBox>;
-export const DefaultCheckBox = () => <CheckBox />;
--- libs/dashboard/components/src/lib/simple/email-input/email-input.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import EmailInput from "./index";
-import { ComponentMeta } from "@storybook/react";
-
-export default {
-  component: EmailInput,
-} as ComponentMeta<typeof EmailInput>;
-export const EmailComponentInput = () => <EmailInput />;
