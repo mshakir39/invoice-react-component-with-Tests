b528ee00fc9b3af824f633e3774e7b84d020295e
Paul Hammant <paul@hammant.org>
2023-03-07 19:44:06 +0000

    contrib of accept button

---
+++ libs/dashboard/components/src/lib/simple/accept-button/accept-button.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import { OverridableStringUnion } from "@mui/types";
+import { debounce } from "lodash";
+
+export interface AcceptButtonProps {
+  text?: string;
+  type?: "submit" | "reset" | "button";
+  color_?: OverridableStringUnion<
+    | "inherit"
+    | "primary"
+    | "secondary"
+    | "success"
+    | "error"
+    | "info"
+    | "warning"
+  >;
+  onClick?: () => void;
+  variant?: "text" | "contained" | "outlined";
+  fullWidth?: boolean;
+  disabled?: boolean;
+  dataTestId?: string;
+  sx?: object;
+}
+
+export const AcceptButton: React.FC<AcceptButtonProps> = ({
+  type = "submit",
+  text = "Default",
+  color_ = "primary",
+  onClick,
+  variant = "contained",
+  fullWidth = true,
+  disabled = false,
+  dataTestId = "accept-btn",
+  sx = {},
+}) => {
+  const doNothing = () => {
+    return;
+  };
+  const debouncedCallback = debounce(onClick || doNothing, 400);
+
+  return (
+    <Button
+      onClick={debouncedCallback}
+      type={type}
+      data-testid={dataTestId}
+      fullWidth={fullWidth}
+      color={color_}
+      variant={variant}
+      disabled={disabled}
+      sx={sx}
+    >
+      {text}
+    </Button>
+  );
+};
+
+export default AcceptButton;
